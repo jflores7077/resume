e60954d929b750a094515c7e14e5f2a8637e48cf
Javier <jflores7077@baysidehighschool.org>
2018-02-16 16:30:24 -0500

    Animated things

--- draft2/main.js
+++ draft2/main.js
@@ -1,13 +1,41 @@
-$(document).ready(function(){
-    $(this).scrollTop(0);
+/* Click about to acess a better "about" page */
+function readAbout(){
     var s = false;
     $('#about').click(function(){
+        
         if(s){
             s= false;
-            $('*').css('overflow-y','hidden');
+            $('html').css('overflow-y','hidden');
+            $('html').animate({
+                'top':'0vh'
+            },800);
+            
+            $(this).css('flex','1')
+            
         }else{
             s = true;
-            $('*').css('overflow-y','initial');
+            $('html').css('overflow-y','initial');
+            
+            $('html').css('position','absolute');
+            $('html').animate({
+                'top':'-90vh'
+            },800);
+            $(this).css('flex','100%')
+            /*
+            $('.items').each(function(){
+                if($(this).attr('id') != 'about'){
+                    $(this).css('color','black');
+                }
+            });
+            */
         }
     });
+}
+/***********************************************/
+
+$(document).ready(function(){
+    //Reset document offset-y to 0
+    $(this).scrollTop(0);
+
+    readAbout();
 });
